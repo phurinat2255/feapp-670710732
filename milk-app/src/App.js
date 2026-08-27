018323bfeace638bbec6ab969ce55bccf61ca8a6
phurinat2255 <pumtra_p2@silpakorn.edu>
2026-08-27 05:26:22 +0000

    React

--- milk-app/src/App.js
+++ milk-app/src/App.js
@@ -1,34 +1,26 @@
-import ProfileCard from './components/ProfileCard';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import About from './pages/About';
+import MovieDetail from './pages/MovieDetail';
+import NotFound from './pages/NotFound';
 
-const members = [
-  { id: 1, name: 'ภูริณัฐ พุ่มทรา', nickname: 'มิ้ว',
-    major: 'เทคโนโลยีสารสนเทศ', favorites: ['เธอ', 'จระเข้'] },
-  // 👉 เพิ่มสมาชิกคนอื่น ๆ ของกลุ่มที่นี่
-
- 
-  { id: 2, name: 'สมชาย ใจร้าย', nickname: 'เมฆ',
-    major: 'วิทยาการคอมพิวเตอร์', favorites: ['กัญชา', 'หมา'] },
-  // 👉 เพิ่มสมาชิกคนอื่น ๆ ของกลุ่มที่นี่
-  { id: 3, name: 'สมศักดิ์ ใจป๋า', nickname: 'ต้นไม้',
-    major: 'วิทยาการข้อมูล', favorites: ['กาแฟ', 'กระทิง'] },
-  // 👉 เพิ่มสมาชิกคนอื่น ๆ ของกลุ่มที่นี่
-];
 function App() {
   return (
-    <div className="container">
-      <h1>สมาชิกกลุ่มของเรา</h1>
-      <div className="card-row">
-        {members.map((m) => (
-          <ProfileCard
-            key={m.id}
-            name={m.name}
-            nickname={m.nickname}
-            major={m.major}
-            favorites={m.favorites}
-          />
-        ))}
-      </div>
+    <div className="flex min-h-screen flex-col bg-slate-50">
+      <Navbar />                {/* ← เห็นทุกหน้า */}
+      <main className="flex-1">
+        <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/movies" element={<Movie />} />
+  <Route path="/movies/:id" element={<MovieDetail />} />
+  <Route path="/about" element={<About />} />
+  <Route path="*" element={<NotFound />} />   {/* ← ดักทุกอย่างที่เหลือ ไว้ล่างสุด */}
+</Routes>
+      </main>
+      <Footer />                {/* ← เห็นทุกหน้า */}
     </div>
   );
 }
